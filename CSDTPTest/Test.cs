58ed59ab7bd4b8498985d2ffdf3313628b74ba6c
WKHAllen <wkhallen@gmail.com>
2022-04-24 17:51:37 -0400

    Add test project

---
+++ CSDTPTest/Test.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSDTPTest
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestCSDTP()
+        {
+            System.Console.WriteLine("Hello, CSDTP!");
+            Assert.IsTrue(false);
+        }
+    }
+}
